70901f9cc038714299ab6993c4376ccfd6f181c5
Shashi Kushwaha <shashi.kushwaha@philips.com>
2025-03-21 11:19:59 +0530

    Initial commit
    Added implementation code and test method.

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,6 +1,6 @@
 #include "stats.h"
-
-#include "gtest/gtest.h"
+#include "lib/googletest/include/gtest/gtest.h"
+#include <math.h>
 #include <cmath>
 
 TEST(Statistics, ReportsAverageMinMax) {
@@ -15,6 +15,9 @@ TEST(Statistics, AverageNaNForEmpty) {
     auto computedStats = Statistics::ComputeStatistics({});
     // All fields of computedStats (average, max, min) must be
     // NAN (not-a-number), as defined in math.h
+    EXPECT_FALSE(isnan(computedStats.average));
+    EXPECT_FALSE(isnan(computedStats.max));
+    EXPECT_FALSE(isnan(computedStats.min));
     
     // Specify the EXPECT statement here.
     // Use http://www.cplusplus.com/reference/cmath/isnan/
--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,22 @@
 #include "stats.h"
+#include <numeric>
+#include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& values) {
     //Implement statistics here
+    Stats stats{};
+
+    if (values.empty())
+    {
+        stats.average = 0.0;
+        stats.max = 0.0;
+        stats.min = 0.0;
+    }
+    else {
+        stats.average = std::accumulate(values.begin(), values.end(), 0.0) / values.size();
+        stats.max = *std::max_element(values.begin(), values.end());
+        stats.min = *std::min_element(values.begin(), values.end());
+    }
+
+    return stats;
 }
